f3cf21e37b98343b4546412ba46b52305df23acf
Sam Pepke <sampepke@Sam-Pepkes-MacBook-Pro.local>
2017-04-02 07:12:33 -0700

    finished makegraph function, worked on printing statistics

--- public/js/heatmap.js
+++ public/js/heatmap.js
@@ -1049,12 +1049,30 @@ function hideIntro() {
   console.log("hey");
   var intro = $("#intro");
   intro.empty();
-  intro.html('<div class="col-xs-12">    <div id="chartHere" class="col-xs-8">       </div>    <div class="col-xs-4">      <p>        Hey      </p>      <p>        Yo      </p>    </div>  </div>');
-  $("#chartHere").append('<canvas id ="lineChart"></canvas>');
-  tempGrapher();
+  intro.html('<div class="row" style="padding-bottom: 15px" style="margin:0 auto"> <div class="col-xs-12" > <div id="chartHere" class="col-xs-8"> </div> <div class="col-xs-4">printStats(data) </div>  </div> </div>');
+  $("#chartHere").append('<canvas id ="lineChart" ></canvas>');
+  var data = [{"week":"2015-06-15 - 2015-06-21", "total": 495},
+ {"week":"2015-06-22 - 2015-06-28", "total": 2540},
+ {"week":"2015-06-29 - 2015-07-05", "total": 2443},
+ {"week":"2015-07-07 - 2015-07-13", "total": 2182},
+ {"week":"2015-07-12 - 2015-07-18", "total": 2411}];
+  makeGraph(data);
+}
+
+function printStats(inputData)
+{
+    var statistics = [];
+    for (var i = 0; i < inputData.length; i++) {
+        console.log(inputData[i]);
+        console.log(inputData[i].total);
+        statistics[i] = inputData[i].total; 
+    }
+
+
 }
 
 function updateGraph() {
-  var graph = document.getElementById('lineChartContainer')
+    var graph = document.getElementById("lineChart")
+    console.log(graph)
     graph.style.display = 'block';
 }
